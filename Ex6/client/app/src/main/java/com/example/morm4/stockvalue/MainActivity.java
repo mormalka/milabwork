2a49f039b9d5c618bb2a7c3adde05ff1bd11f3ed
mormalka <morm4332@gmail.com>
2019-01-08 12:55:01 +0200

    client side on progress

--- Ex6/client/app/src/main/java/com/example/morm4/stockvalue/MainActivity.java
+++ Ex6/client/app/src/main/java/com/example/morm4/stockvalue/MainActivity.java
@@ -2,33 +2,89 @@ package com.example.morm4.stockvalue;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+
+import android.app.Application;
+import android.widget.Toast;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.net.URISyntaxException;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final String URL = "http://10.0.2.2:3000";
+
+    private Socket sSocket;
+    {
+        try {
+            sSocket = IO.socket(URL);
+            Log.d("hello", "Connection Done");
+        } catch (URISyntaxException e) {
+            Log.d("hello", "Unable to connect");
+            throw new RuntimeException(e);
+        }
+    }
 
     String stockType;
     Button sendButt;
-    EditText stockID;
+    EditText stock;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Toast.makeText(getApplicationContext(),"Live Stock Exchange", Toast.LENGTH_LONG).show();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        stockID= (EditText)findViewById(R.id.editText);
-        sendButt= (Button)findViewById(R.id.buttonSend);
+        stock = (EditText) findViewById(R.id.editText);
+        sendButt = (Button) findViewById(R.id.buttonSend);
 
-        sendButt.setOnClickListener(new View.OnClickListener(){
+        sendButt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                stockType = stockID.getText().toString();
+                stockType = stock.getText().toString();
 
             }
-
         });
+        sSocket.on("connection", sendStock);
+    }
+    private Emitter.Listener sendStock = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            MainActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if(!stockType.equals("")) {
+                        sSocket.emit("stock type", stockType);
+                        Toast.makeText(getApplicationContext(),
+                                "sending stock type to server", Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }
+    };
+
+//        private Emitter.Listener showPrice = new Emitter.Listener() {
+//            @Override
+//            public void call(Object... args) {
+//                JSONObject data = (JSONObject) args[0];
+//
+//                String price;
+//                try {
+//                    price = data.getString("price");
+//                } catch (JSONException e) {
+//                    return;
+//                }
+//            }
+//        };
 
 
     }
-}
+
--- Ex6/server/alphaServer.js
+++ Ex6/server/alphaServer.js
@@ -8,24 +8,30 @@ const app = express();
 
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const port = process.env.PORT || 3000;
+
+server.listen(port, function () {
+	console.log('Server listening at port %d', port);
+});
+
+
+app.use(express.static(__dirname + '/public'));
 
 
 io.on('connection', function (socket) {
-    // when the client emits 'share sub', this listens and executes
-    socket.on('stock type', function (inc) {
-
+    // when the client emits 'stock type', this listens and executes
+    socket.on('stock type', function (type) {
+        console.log('io recognize event');
         setInterval(() => {
             alpha.data.batch([`${type}`]).then(data => {
-            let ss = `${type}`; 
             let value = `${data['Stock Quotes'][0]['2. price']}`;
             console.log(`${type} -> ${data['Stock Quotes'][0]['2. price']}`);
             //console.dir(data);
 
             // returns the price value of the selected share subject
 		    socket.broadcast.emit('stock type', { 
-                stock type : ss
-                price value : value
-             });
+                price : value
+            });
 
             }).catch(err => {
                 console.error("Error: " + err);
@@ -34,11 +40,10 @@ io.on('connection', function (socket) {
         //calls every 15 sec
         }, 15000);
 
-
     });
 });
 
-server.listen(3000);
+
 
 
 
